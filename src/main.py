48f6d7a4bded2aeb756a5361c420b6e03302f50d
SHUMIKAZE <shumikaze@proton.me>
2026-01-22 23:31:14 +0300

    Added running script

--- src/main.py
+++ src/main.py
@@ -2,14 +2,6 @@ from pathlib import Path
 from . import db_package as db
 from .command_handler import MainCommandHandler
 
-DB_PATH = Path("media.db")
-SCHEMA_PATH = Path("src/db_package/schema.sql")
-
-print("Connection...")
-DB = db.init(DB_PATH, SCHEMA_PATH)
-
-print("App started.")
-
 def action_list(db_conn, action):
     data = db.get_data(db_conn, action["target"])
     for r in data:
@@ -22,30 +14,40 @@ def action_quit(db_conn, _):
     db.close(db_conn)
     raise SystemExit
 
-main_handler = MainCommandHandler()
+def run():
+    DB_PATH = Path("media.db")
+    SCHEMA_PATH = Path("src/db_package/schema.sql")
 
-while True:
-    try:
-        user_input = input(">>> ")
-    except KeyboardInterrupt:
-        user_input = "quit"
-    except EOFError:
-        user_input = "quit"
+    print("Connection...")
+    DB = db.init(DB_PATH, SCHEMA_PATH)
 
-    action = main_handler.handle(user_input)
+    print("App started.")
 
 
-    if action is None:
-        continue
+    main_handler = MainCommandHandler()
 
-    func_name = "action_" + action["type"]
-    handler = globals().get(func_name)
+    while True:
+        try:
+            user_input = input(">>> ")
+        except KeyboardInterrupt:
+            user_input = "quit"
+        except EOFError:
+            user_input = "quit"
 
-    if not handler:
-        print("Unknown action type:", action["type"])
-        continue
+        action = main_handler.handle(user_input)
 
-    try:
-        handler(DB, action)
-    except SystemExit:
-        break
+
+        if action is None:
+            continue
+
+        func_name = "action_" + action["type"]
+        handler = globals().get(func_name)
+
+        if not handler:
+            print("Unknown action type:", action["type"])
+            continue
+
+        try:
+            handler(DB, action)
+        except SystemExit:
+            break
